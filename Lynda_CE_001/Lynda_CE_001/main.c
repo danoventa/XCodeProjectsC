8545a82f2cf7b7bff4879121f33c098a578aec11
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-19 15:00:08 -0500

    Playing with C

---
+++ Lynda_CE_001/Lynda_CE_001/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Lynda_CE_001
+//
+//  Created by Daniel A. Noventa on 5/19/15.
+//  Copyright (c) 2015 Noventa Foundation. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    print("please enter a number!");
+    return 0;
+}
